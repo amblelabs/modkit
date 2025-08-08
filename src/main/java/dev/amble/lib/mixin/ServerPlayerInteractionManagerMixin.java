65cd702e3f66481463807f195a0ebd175aed90e9
MaketendoDev <maketendomail@gmail.com>
2025-08-08 23:50:05 +0100

    feat: extended ICantBreak to show the player who attempted to break the block. #34

--- src/main/java/dev/amble/lib/mixin/ServerPlayerInteractionManagerMixin.java
+++ src/main/java/dev/amble/lib/mixin/ServerPlayerInteractionManagerMixin.java
@@ -1,5 +1,6 @@
 package dev.amble.lib.mixin;
 
+import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,11 +20,14 @@ public class ServerPlayerInteractionManagerMixin {
     @Shadow
     protected ServerWorld world;
 
+    @Shadow
+    protected ServerPlayerEntity player;
+
     @Inject(method = "tryBreakBlock", at = @At(value = "HEAD"), cancellable = true)
     public void ait$tryBreakBlock(BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
         Block block = this.world.getBlockState(pos).getBlock();
         if (block instanceof ICantBreak cantBreak) {
-            cantBreak.onTryBreak(this.world, pos, this.world.getBlockState(pos));
+            cantBreak.onTryBreak(this.world, pos, this.world.getBlockState(pos), this.player);
             cir.setReturnValue(false);
             cir.cancel();
         }
