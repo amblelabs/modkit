77d9e64939c91f65fb70d19ad0516a1cc8ca3aa7
Addi3 <122154427+Addi3@users.noreply.github.com>
2025-10-22 23:03:01 +0100

    feat: example blocks for new tags

--- src/test/java/dev/amble/litmus/block/LitmusBlocks.java
+++ src/test/java/dev/amble/litmus/block/LitmusBlocks.java
@@ -6,5 +6,14 @@ import dev.amble.litmus.block.impl.TestBlock;
 import net.minecraft.block.Block;
 
 public class LitmusBlocks extends BlockContainer {
+
 	public static final Block TEST_BLOCK = new TestBlock(new ABlockSettings());
+
+	public static final Block TEST_HOE_BLOCK = new Block(new ABlockSettings());
+
+
+	public static final Block TEST_AXE_BLOCK = new Block(new ABlockSettings());
+
+
+	public static final Block TEST_SHOVEL_BLOCK = new Block(new ABlockSettings());
 }
---
+++ src/test/java/dev/amble/litmus/datagen/LitmusModDataGenerator.java
@@ -0,0 +1,28 @@
+package dev.amble.litmus.datagen;
+
+import dev.amble.lib.datagen.loot.AmbleBlockLootTable;
+import dev.amble.lib.datagen.sound.AmbleSoundProvider;
+import dev.amble.lib.datagen.tag.AmbleBlockTagProvider;
+import dev.amble.litmus.block.LitmusBlocks;
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LitmusModDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator gen) {
+        FabricDataGenerator.Pack pack = gen.createPack();
+
+        genLoot(pack);
+        genTags(pack);
+    }
+
+    private void genTags(FabricDataGenerator.Pack pack) {
+        pack.addProvider((((output, registriesFuture) -> new AmbleBlockTagProvider(output, registriesFuture).withBlocks(LitmusBlocks.class))));
+    }
+
+    private void genLoot(FabricDataGenerator.Pack pack) {
+        pack.addProvider((((output, registriesFuture) -> new AmbleBlockLootTable(output).withBlocks(LitmusBlocks.class))));
+    }
+
+
+}
